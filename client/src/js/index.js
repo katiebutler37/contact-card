d1956bf6b90896b791e284d36a540e512acb0736
Katie <katiebutler37@gmail.com>
2022-09-11 19:22:19 -0400

    debugging heroku

---
+++ client/src/js/index.js
@@ -0,0 +1,19 @@
+// Import modules
+import  "./form";
+import "./submit";
+
+// Import CSS files
+import "../css/index.css";
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Tooltip, Toast, Popover } from 'bootstrap';
+
+import Logo from '../images/logo.png';
+import Bear from '../images/bear.png';
+import Dog from '../images/dog.png';
+
+window.addEventListener('load', function () {
+    document.getElementById('logo').src = Logo;
+    document.getElementById('bearThumbnail').src = Bear;
+    document.getElementById('dogThumbnail').src = Dog;
+  });
