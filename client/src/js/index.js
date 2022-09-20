ff4fb37cbfeb10a39f72e7ca8684a7b421555c9c
Katie <katiebutler37@gmail.com>
2022-09-19 22:45:48 -0400

    merge

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -28,12 +28,28 @@ window.addEventListener('load', function () {
 // Form functionality
 const form = document.getElementById("formToggle");
 const newContactButton = document.getElementById("new-contact");
+const installBtn = document.getElementById("installBtn");
 let submitBtnToUpdate = false;
 let profileId;
 
 newContactButton.addEventListener('click', event => {
  toggleForm()
 })
+
+window.addEventListener('beforeinstallprompt', (event) => {
+  event.preventDefault();
+  installBtn.style.visibility = 'visible';
+
+  installBtn.addEventListener('click', () => {
+    event.prompt();
+    installBtn.setAttribute('disabled', true);
+    installBtn.textContent = 'Installed!';
+    });
+  });
+
+  window.addEventListener('appinstalled', (event) => {
+    console.log('👍', 'appinstalled', event);
+  });  
 
 form.addEventListener('submit', (event) => {
   // handle the form data
